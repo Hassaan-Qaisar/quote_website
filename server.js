a1e6edde78cf699ebccad428b757fac2379aef6b
Hassaan-Qaisar <106833785+Hassaan-Qaisar@users.noreply.github.com>
2023-09-24 01:46:13 +0500

    Update server.js

--- server.js
+++ server.js
@@ -20,10 +20,6 @@ const port = process.env.PORT || 4000;
 const app = express();
 const httpServer = http.createServer(app);
 
-if (process.env.NODE_ENV !== "production") {
-  dotenv.config();
-}
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -65,17 +61,10 @@ const server = new ApolloServer({
   ],
 });
 
-// if(process.env.NODE_ENV=="production"){
-  // app.use(express.static('client/build'))
-  // app.get("*",(req,res)=>{
-  //     res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  // })
-// }
-
-app.get("/", (req, res) => {
-  res.send("booom");
-});
-
+  app.use(express.static('client/build'))
+  app.get("*",(req,res)=>{
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
 
 await server.start();
 server.applyMiddleware({
